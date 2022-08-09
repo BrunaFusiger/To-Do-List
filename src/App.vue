a2ed40143b12bce13644ba978a64602e670c120c
Bruna <bruna.fusiger@gmail.com>
2022-08-09 15:38:08 -0300

    initializing project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+
+  <div>
+    <Base />
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Base from "./components/Base.vue";
 
-nav {
-  padding: 30px;
+export default {
+  name: "Home",
+  components: {
+    Base
+  }
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    background-image: linear-gradient(to right, #000355, #000);
 }
 </style>
---
+++ src/components/Base.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="base">
+        <nav id="base-nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Base"
+}
+</script>
+
+<style scoped lang="scss">
+#base {
+    background: red;
+
+    &-nav {
+        background: yellow;
+    }
+}
+</style>
