3879f036ffe5970092d4a2486e77032b780a9543
Bruna <bruna.fusiger@gmail.com>
2022-08-10 15:20:31 -0300

    adding todo routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Home.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'all',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/done',
+    name: 'done',
+    component: Home
+  },
+  {
+    path: '/undone',
+    name: 'undone',
+    component: Home
+  },
 ]
 
 const router = createRouter({
